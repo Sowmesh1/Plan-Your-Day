3563e7354ca96bc74f3d35ad9141e4d0521495f9
Sowmesh <sowmeshmuthu2@gmail.com>
2026-02-16 21:59:48 +0530

    Add initial UI skeleton for main screen

---
+++ app/src/main/java/com/example/planyourday/DailyPlannerFragment.java
@@ -0,0 +1,40 @@
+package com.example.planyourday;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DailyPlannerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DailyPlannerFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    public DailyPlannerFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_daily_planner, container, false);
+    }
+
+}
